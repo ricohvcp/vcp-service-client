0aba25b9449864153dd3dac8f3820939450c04b0
ysuke <yusuke-k@nts.ricoh.co.jp>
2015-02-26 16:23:04 +0900

    update copyright

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * https://github.com/ricohvcp/ucs-service-client
  *
  * Licensed under the MIT license.
- * Copyright (c) 2015 Ricoh
+ * Copyright (c) 2015 Ricoh Company, Ltd. All Rights Reserved.
  */
 
 module.exports = require('./src');
